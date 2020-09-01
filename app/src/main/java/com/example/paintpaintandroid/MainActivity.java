7faf4308f2e14c8aa6b474b34d134c37a0e9a05b
Evan <evannwong>
2020-09-01 02:59:40 -0600

    Finished MainActivity.java and ran bug tests. Fixed button layout overlap and fixed drawing area bug where the drawing area would not resize correctly. Should now be compatible on almost all devices.

---
+++ app/src/main/java/com/example/paintpaintandroid/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.paintpaintandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        PaintView paintView = new PaintView(this);
+        setContentView(paintView);
+    }
+}
